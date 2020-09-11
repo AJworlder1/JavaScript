b49e035dfd4b8290c9f98ebef63f9894493078e5
unknown <none@ee.ee>
2020-09-11 09:00:25 +0300

    Заготовка проекта

---
+++ src/sptvr19_calculator/SPTVR19_calculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19_calculator;
+
+/**
+ *
+ * @author user
+ */
+public class SPTVR19_calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("--- Калькулятор ---");
+        // TODO code application logic here
+    }
+    
+}
